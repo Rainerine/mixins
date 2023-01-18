8f3dbef55210cf5f3a6838449595dc1a1b8da18c
Katie Phillips <katiephillips21@gmail.com>
2023-01-18 17:21:29 -0500

    Init Push

---
+++ README.md
@@ -0,0 +1,4 @@
+# My Mixin List
+By Katie Phillips for all CSS Bootstrap Projects
+
+A helpful set of CSS mixins I use with many projects and my basic javascript framework.
---
+++ js/globals.js
@@ -0,0 +1,9 @@
+(function ($) {
+    let globalScripts = function () {
+        //Add Functions here
+        return {};
+    }
+    $( document ).ready(function() {
+        //Init Functions
+    });
+})(jQuery);
